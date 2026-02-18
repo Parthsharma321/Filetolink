ff6702318dc5391c9998fedcdf6909548c023fd0
Parthsharma321 <0612parth@gmail.com>
2026-02-18 20:19:41 +0530

    Update translation.py

--- FileStream/utils/translation.py
+++ FileStream/utils/translation.py
@@ -20,7 +20,7 @@ class LANG(object):
 <b>⚜ ᴍʏ ɴᴀᴍᴇ : {}</b>\n
 <b>✦ ᴠᴇʀsɪᴏɴ : {}</b>
 <b>✦ ᴜᴘᴅᴀᴛᴇᴅ ᴏɴ : 06-January-2024</b>
-<b>✦ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://telegram.me/Parthji55'>Avishkar Patil</a></b>\n
+<b>✦ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://telegram.me/Parthji55'>Parth Sharma</a></b>\n
 """
 
     STREAM_TEXT = """
